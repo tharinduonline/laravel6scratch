67b06920fbe4b39288bf0af55c301b8da30d9f97
Tharindu Chathuranga <tharindu@serverclub.lk>
2020-05-05 22:13:02 +0530

    Initial commit

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                   <?= $name; ?>
+                    <?= htmlspecialchars($name, ENT_QUOTES); ?>
                 </div>
 
             </div>
